cb9bab71e89bbd7715fbd62bfd09bd594103db71
Vijesh Shetty <vijeshsshetty@gmail.com>
2023-08-08 20:00:01 +0530

    Env
    Co-Authored-By: Vignesh Shetty <vigneshshetty.in@gmail.com>

--- rssbuddy/__init__.py
+++ rssbuddy/__init__.py
@@ -3,14 +3,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from os import environ
 
 app = Flask(__name__)
 
-
-POSTGRES_USER = ''
-POSTGRES_PW = ''
-POSTGRES_URL = ''
-POSTGRES_DB = ''
+POSTGRES_USER = environ.get('POSTGRES_USER')
+POSTGRES_PW = environ.get('POSTGRES_PW')
+POSTGRES_URL = environ.get('POSTGRES_URL')
+POSTGRES_DB = environ.get('POSTGRES_DB')
 
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 
